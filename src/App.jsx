8c949b124a6b0489240aa3916b185420f126e863
Mehdi Ayeb <mehdiayeb14@gmail.com>
2026-07-12 05:57:47 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Weather from "./components/Weather"
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="app">
+ 
+      <Weather />
+
+    </div>
+    </>
+  )
+}
+
+export default App
